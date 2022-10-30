e27606421ae27ca161370ee6701fe1e948a09f0a
miguel <miguel@cinaq.com>
2022-10-30 20:18:03 +0100

    fix static files loading not working

--- src/imagesClassifier/settings.py
+++ src/imagesClassifier/settings.py
@@ -20,7 +20,7 @@ DATA_UPLOAD_MAX_NUMBER_FIELDS = None
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 #BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -151,9 +151,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
-STATIC_URL = 'static/'
 
 # Enable WhiteNoise's GZip compression of static assets.
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
