22718509234ba07339f618835dff35bd2d2ec7bd
miguel <miguel@cinaq.com>
2022-10-30 21:19:48 +0100

    trying to fix db issue on heroku

--- src/imagesClassifier/settings.py
+++ src/imagesClassifier/settings.py
@@ -14,6 +14,7 @@ import os
 import dj_database_url
 from django.test.runner import DiscoverRunner
 from pathlib import Path
+import dotenv
 
 # to disable the check
 DATA_UPLOAD_MAX_NUMBER_FIELDS = None
@@ -94,25 +95,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'imagesClassifier.wsgi.application'
 
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
+
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-if "DATABASE_URL" in os.environ:
-    # Configure Django for DATABASE_URL environment variable.
-    DATABASES["default"] = dj_database_url.config(
-        conn_max_age=600, ssl_require=True)
-
-    # Enable test database if found in CI environment.
-    if "CI" in os.environ:
-        DATABASES["default"]["TEST"] = DATABASES["default"]
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 
 # Password validation
