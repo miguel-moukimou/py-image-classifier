3ffae2ce0fa0a85de7478c756d11182e645a81f7
miguel <miguel@cinaq.com>
2022-10-30 12:23:21 +0100

    created navigation bar, added react router

--- frontend-classifier/src/components/Navigation/Navigation.js
+++ frontend-classifier/src/components/Navigation/Navigation.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
 const Navigation = () => {
     return (
         <Navbar bg="dark" variant="dark" className='mb-3'>
-            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            <Navbar.Brand href="/"></Navbar.Brand>
             <Nav className="me-auto">
                 <Nav.Link href="/">Home</Nav.Link>
                 <Nav.Link href="/list">Images</Nav.Link>
